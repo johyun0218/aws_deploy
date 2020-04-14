634f9115b78c7a89e9ed90fce218090955dbd89b
mac <johyun0218@naver.com>
2020-04-14 22:22:06 +0900

    production

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,7 @@
 # # get node into yum
 # curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 # # install node and npm in one line
-yum install -y nodejs
+#yum install -y nodejs
 # # install pm2 to restart node app
 # npm i -g pm2@2.4.3
+npm install --production
