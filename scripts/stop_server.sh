d3b40a94d93eb7b14daad54a4779e1bf3c31fd7b
mac <johyun0218@naver.com>
2020-04-15 21:24:42 +0900

    주석제거

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-#sudo pm2 stop node-app
-
 sudo service nginx stop
-
-# isExistApp=`sudo pgrep pm2`
-# if [[ -n  $isExistApp ]]; then
-    # sudo pm2 kill
-    # sudo pm2 stop node-app
-    # you can remove the crontab as well
-# fi
-
-# isExistApp=`pgrep pm2`
-# if [[ -n  $isExistApp ]]; then
-#     # sudo pm2 kill
-#     echo 'yo'
-#     # you can remove the crontab as well
-# fi
